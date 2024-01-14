2fb22c73f92eb96e6984556969e824b32a4ba768
omerislamaz <omerislamaz@gmail.com>
2024-01-15 00:20:22 +0300

    Ders 28 sonu

---
+++ MyAcademyCarBook.BusinessLayer/ValidationRules/ServiceValidation/CreateServiceValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MyAcademyCarBook.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAcademyCarBook.BusinessLayer.ValidationRules.ServiceValidation
+{
+    public class CreateServiceValidator: AbstractValidator<Service>
+    {
+        public CreateServiceValidator()
+        {
+            RuleFor(x=>x.Title).NotEmpty().WithMessage("Başlığı boş geçemezsiniz!");
+            RuleFor(x=>x.Descriptipon).NotEmpty().WithMessage("Açıklama alanını boş geçemezsiniz!");
+            RuleFor(x=>x.Icon).NotEmpty().WithMessage("Ikon alanını boş geçemezsiniz!");
+            RuleFor(x=>x.Title).MinimumLength(3).WithMessage("Lütfen en az 3 karakter veri girişi yapınız!");
+            RuleFor(x=>x.Title).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter veri girişi yapınız!");
+            RuleFor(x=>x.Descriptipon).MinimumLength(10).WithMessage("Lütfen en az 10 karakter veri girişi yapınız!");
+            RuleFor(x=>x.Descriptipon).MaximumLength(200).WithMessage("Lütfen en fazla 200 karakter veri girişi yapınız!");
+        }
+    }
+}
--- MyAcademyCarBook.PresentationLayer/Controllers/ServiceController.cs
+++ MyAcademyCarBook.PresentationLayer/Controllers/ServiceController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using MyAcademyCarBook.BusinessLayer.Abstract;
+using MyAcademyCarBook.BusinessLayer.ValidationRules.ServiceValidation;
+using MyAcademyCarBook.EntityLayer.Concrete;
 
 namespace MyAcademyCarBook.PresentationLayer.Controllers
 {
-	public class ServiceController : Controller
-	{
-		private readonly IServiceService _serviceService;
+    public class ServiceController : Controller
+    {
+        private readonly IServiceService _serviceService;
 
         public ServiceController(IServiceService serviceService)
         {
@@ -13,9 +16,40 @@ namespace MyAcademyCarBook.PresentationLayer.Controllers
         }
 
         public IActionResult Index()
-		{
-			var values = _serviceService.TGetListAll();
-			return View(values);
-		}
-	}
+        {
+            var values = _serviceService.TGetListAll();
+            return View(values);
+        }
+
+        public IActionResult ServiceList()
+        {
+            var values = _serviceService.TGetListAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateService(Service service)
+        {
+            CreateServiceValidator validationRules = new CreateServiceValidator();
+            ValidationResult result = validationRules.Validate(service);
+            if (result.IsValid)
+            {
+                _serviceService.TInsert(service);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+        }
+    }
 }
